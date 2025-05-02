3e382e40267b567539a5a43607f11fe0ab7e4fed
Joao Gabriel <123424925+artesssn@users.noreply.github.com>
2025-05-02 14:49:23 -0300

    Add files via upload

---
+++ Squid Game/Squid Game.py
@@ -0,0 +1,25 @@
+import random
+import os
+
+number = random.randint(1, 5)
+guess = input("Escolha um número entre 1 e 5: ")
+guess = int(guess)
+
+if guess == number:
+    print("Você acertou!")
+else:
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("erro")
+    print("Voce perdeu! removendo a system32")
+    
